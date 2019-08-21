3141668232d39243daf11f3b8ceab4cd9bd249b5
Calvin Foster <cal.j.foster@gmail.com>
2019-08-20 20:32:15 -0500

    my cool code is complete

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myThing = new { Stuff = "Calvin", otherproperty = 123 };
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(myThing.Stuff);
+        }
+    }
+}
